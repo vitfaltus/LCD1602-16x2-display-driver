8f94fb75a056a032b958f9588b991327cefa3d8c
Vit Faltus <vit.faltus@proton.me>
2026-06-23 20:48:51 +0200

    Declaration of LCD class and some of its methods, definition of LCD constructor

--- include/LCD.h
+++ include/LCD.h
@@ -1,8 +1,45 @@
 #ifndef LCD1602_16X2_DISPLAY_DRIVER_LCD_H
 #define LCD1602_16X2_DISPLAY_DRIVER_LCD_H
+#include <stdint.h>
+#include <Arduino.h>
+
+
 
 class LCD
 {
+  enum PinOrder {
+    Enable,
+    RW,
+    RS,
+    D0,
+    D1,
+    D2,
+    D3,
+    D4,
+    D5,
+    D6,
+    D7,
+  };
+
+  int Bus8Bit[4];
+  int Bus4Bit[4];
+  int EnablePin;
+  int RWPin;
+  int RSPin;
+
+  bool PinStates[10];
+
+  // Sets pins according PinStates array with indexing of PinOrder enum.
+  void setPins();
+
+
+public:
+  LCD(int RS, int RW, int E, int D0,int D1,int D2,int D3, int D4, int D5, int D6, int D7);
+  void clear();
+  void setCursor(uint8_t x, uint8_t y);
+  void write(char c);
+  void backLight(bool enable);
+
 };
 
 
