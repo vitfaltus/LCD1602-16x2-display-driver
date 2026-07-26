c10c9616952dd777b391e72216f46f9e241ac006
Vit Faltus <vit.faltus@proton.me>
2026-07-26 15:00:03 +0200

    declared and defined basic private functions, created constructor and clearDisplay method

--- include/LCD1602A16x2.h
+++ include/LCD1602A16x2.h
@@ -1,40 +1,47 @@
 #ifndef LCD1602_16X2_DISPLAY_DRIVER_LCD_H
 #define LCD1602_16X2_DISPLAY_DRIVER_LCD_H
 #include <stdint.h>
-#define NUMBER_OF_PINS 11
+#define BUS_PINS 8
+
+// commands
+#define LCD_CLEARDISPLAY 0x01
+#define LCD_RETURNHOME 0x02
+#define LCD_ENTRYMODESET 0x04
+#define LCD_DISPLAYCONTROL 0x08
+#define LCD_CURSORSHIFT 0x10
+#define LCD_FUNCTIONSET 0x20
+#define LCD_SETCGRAMADDR 0x40
+#define LCD_SETDDRAMADDR 0x80
 
 
 class LCD1602A16x2
 {
-  enum PinOrder {
-    EnPin,
-    RSPin,
-    RWPin,
-    D0Pin,
-    D1Pin,
-    D2Pin,
-    D3Pin,
-    D4Pin,
-    D5Pin,
-    D6Pin,
-    D7Pin,
-  };
 
-  int PinNames[NUMBER_OF_PINS]{};
+  uint8_t EnablePin;
+  uint8_t RWPin;
+  uint8_t RSPin;
 
-  bool PinStates[NUMBER_OF_PINS]{};
-
-
+  uint8_t BusNames[BUS_PINS];
 
   void pulseEnable() const;
+
+  void waitForNotBusy() const;
+
+  void setBusPins(uint8_t data) const;
+
+  void sendData(uint8_t data) const;
+
 
 
 
 public:
-  LCD1602A16x2(int RS, int RW, int E, int D0,int D1,int D2,int D3, int D4, int D5, int D6, int D7);
-
+  LCD1602A16x2(uint8_t RS, uint8_t RW, uint8_t E, uint8_t D0,uint8_t D1,uint8_t D2,uint8_t D3, uint8_t D4, uint8_t D5, uint8_t D6, uint8_t D7);
+  void clearDisplay() const;
+  void returnHome() const;
 
 };
 
 
 #endif //LCD1602_16X2_DISPLAY_DRIVER_LCD_H
+
+//00000001
--- src/LCD1602A16x2.cpp
+++ src/LCD1602A16x2.cpp
@@ -2,30 +2,70 @@
 #include <Arduino.h>
 
 
-LCD1602A16x2::LCD1602A16x2(int RS, int RW, int E, int D0, int D1, int D2, int D3, int D4, int D5,
-         int D6, int D7) {
+LCD1602A16x2::LCD1602A16x2(uint8_t RS, uint8_t RW, uint8_t E, uint8_t D0, uint8_t D1, uint8_t D2, uint8_t D3, uint8_t D4, uint8_t D5,
+         uint8_t D6, uint8_t D7) {
 
-  PinNames[EnPin] = E;
+  EnablePin = E;
+  RWPin = RW;
+  RSPin = RS;
 
-  PinNames[RSPin] = RS;
-  PinNames[RWPin] = RW;
+  BusNames[0] = D0;
+  BusNames[1] = D1;
+  BusNames[2] = D2;
+  BusNames[3] = D3;
+  BusNames[4] = D4;
+  BusNames[5] = D5;
+  BusNames[6] = D6;
+  BusNames[7] = D7;
 
+  pinMode(EnablePin, OUTPUT);
+  pinMode(RWPin, OUTPUT);
+  pinMode(RSPin, OUTPUT);
 
-  PinNames[D0Pin] = D0;
-  PinNames[D1Pin] = D1;
-  PinNames[D2Pin] = D2;
-  PinNames[D3Pin] = D3;
+  for (unsigned char BusName : BusNames) {
+    pinMode(BusName, OUTPUT);
+  }
 
-  PinNames[D4Pin] = D4;
-  PinNames[D5Pin] = D5;
-  PinNames[D6Pin] = D6;
-  PinNames[D7Pin] = D7;
-
-  
-
-
+}
+void LCD1602A16x2::clearDisplay() const {
+  sendData(LCD_CLEARDISPLAY);
 }
 
 void LCD1602A16x2::pulseEnable() const {
+  digitalWrite(EnablePin, LOW);
+  delayMicroseconds(1);
+  digitalWrite(EnablePin, HIGH);
+  delayMicroseconds(1);
+  digitalWrite(EnablePin, LOW);
+  delayMicroseconds(1);
+}
+void LCD1602A16x2::waitForNotBusy() const {
+  pinMode(BusNames[7], INPUT);
+  digitalWrite(RWPin, HIGH);
+  digitalWrite(RSPin, LOW);
+  pulseEnable();
+  while (digitalRead(BusNames[7]) == HIGH) {
+    pulseEnable();
+  }
+  pinMode(BusNames[7], OUTPUT);
 
+}
+void LCD1602A16x2::setBusPins(uint8_t data) const {
+  for (uint_fast8_t i = 0; i < BUS_PINS; i++) {
+    if (data % 2 == 0) {
+      digitalWrite(BusNames[i], LOW);
+    }
+    else {
+      digitalWrite(BusNames[i], HIGH);
+    }
+    data >>= 1;
+  }
+}
+
+
+void LCD1602A16x2::sendData(const uint8_t data) const {
+  waitForNotBusy();
+  setBusPins(data);
+  delayMicroseconds(1);
+  pulseEnable();
 }
